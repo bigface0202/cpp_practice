b06b91918af2dca9b889253c18a38c31a3d7530a
Yusuke <theshoreunder@gmail.com>
2021-07-04 16:53:34 +0900

    update Bazelによるビルドファイルの追加|OpenCVの環境構築

---
+++ opencv/practice1/Main/sample.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file sample.cpp
+ *
+ * @author ssugino / created on 2021/06/30
+ * @copyright 2021 GlobalWalkers.inc. All rights reserved.
+ */
+#include <stdio.h>
+
+#include "Main/VideoIO/VideoCapture.h"
+#include "Main/VideoIO/VideoWriter.h"
+
+int main( int argc, char *argv[] )
+{
+    printf("test");
+}
