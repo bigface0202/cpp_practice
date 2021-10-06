eaad0ecb1bfdb3b4966c0edda62e0d97726a7924
Yusuke <theshoreunder@gmail.com>
2021-10-06 09:47:53 +0900

    Q48 要素列の最頻出要素

---
+++ The-Modern-C++Challenge/Question48/Main/answer.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file answer.cpp
+ * 要素列の最頻出要素
+ * 
+ * @author bigface / created on 2021/10/06
+ */
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+
+/** 
+ * 解答
+ * - 各要素の出現回数をstd::mapに記録する。キーが要素で、値が出現回数
+ * - std::max_element()を用いて出現回数最大の要素を求める。結果の型は連想記憶の要素であり、要素と出現回数のペア。
+ * - 最大要素の値と等しい値を持つ全マップ要素をコピーして、最終結果として返す。
+*/
+
+template <typename T>
+std::vector<std::pair<T, size_t>> find_most_frequent(std::vector<T> const & range)
+{
+  std::map<T, size_t> counts;
+  
+  // countsというmapを作り、eがkeyで、同じkeyに対してインクリメントしている
+  // 配列をインクリメントしてるように見えて若干違和感あるけど、
+  // デバッグで中身見ると確かにな、という感じ
+  for (auto const & e : range) counts[e]++;
+
+  auto maxelem = std::max_element(
+    std::cbegin(counts), std::cend(counts),
+    [](auto const & e1, auto const & e2){
+      return e1.second < e2.second;
+    }
+  );
+
+  std::vector<std::pair<T, size_t>> result;
+
+  std::copy_if(
+    std::cbegin(counts), std::cend(counts),
+    std::back_inserter(result),
+    [maxelem](auto const & kvp) {
+      return kvp.second == maxelem->second;
+    }
+  );
+
+  return result;
+}
+
+int main( int argc, char *argv[] )
+{
+  auto range = std::vector<int>{1, 1, 3, 5, 8, 13, 3, 5, 8, 8, 5};
+  auto result = find_most_frequent(range);
+
+  for (auto const & [e, count] : result)
+  {
+    std::cout << e << ":" << count << std::endl;
+  }
+}
