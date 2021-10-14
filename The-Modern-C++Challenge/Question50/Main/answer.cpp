46a189b7a20124fa371d272e0d4b681144373535
Yusuke <theshoreunder@gmail.com>
2021-10-14 09:08:36 +0900

    Q50 コメントの追加

--- The-Modern-C++Challenge/Question50/Main/answer.cpp
+++ The-Modern-C++Challenge/Question50/Main/answer.cpp
@@ -15,9 +15,50 @@
 */
 bool starts_with(std::string_view str, std::string_view prefix)
 {
+  // findは見つかれば、その位置を返す
   return str.find(prefix) == 0;
 }
 
+template <typename InputIt>
+std::vector<std::string> filter_numbers(InputIt begin, InputIt end, std::string const & countryCode)
+{
+  std::vector<std::string> result;
+  // back_inserterはback_insert_iteratorのヘルパ関数
+  // back_insert_iteratorは出力イテレータであり、代入の際にコンテナのpush_back()メンバ関数を呼び出すイテレータアダプタ
+  std::copy_if(
+    begin, end,
+    std::back_inserter(result),
+    [countryCode](auto const & number) {
+      return starts_with(number, countryCode) || starts_with(number, "+" + countryCode);
+    }
+  );
+  return result;
+}
+
+// filter_numbersのWrapper
+std::vector<std::string> filter_numbers(std::vector<std::string> const & numbers, std::string const & countryCode)
+{
+  return filter_numbers(std::cbegin(numbers), std::cend(numbers), countryCode);
+}
+
+int main()
+{
+  std::vector<std::string> numbers{
+    "+4074909080",
+    "44 7520 112233",
+    "+44 7555 123456",
+    "40 7200 123456",
+    "4412345665",
+    "7555 123456"
+  };
+
+  auto result = filter_numbers(numbers, "44");
+
+  for (auto const & number : result)
+  {
+    std::cout << number << std::endl;
+  }
+}
 
 // 正規表現を用いたフィルタリング
 // int main( int argc, char *argv[] )
