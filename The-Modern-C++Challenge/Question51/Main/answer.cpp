2d7afe0cdb81c81e50310686cfe0c1c14d80a9e2
Yusuke <theshoreunder@gmail.com>
2021-10-21 14:05:41 +0900

    Q51 再帰を使わない順列を実装

--- The-Modern-C++Challenge/Question51/Main/answer.cpp
+++ The-Modern-C++Challenge/Question51/Main/answer.cpp
@@ -8,32 +8,55 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <algorithm>
 
+/** 
+ * 解答
+*/
+void print_permutation(std::string str)
+{
+  std::sort(std::begin(str), std::end(str));
+
+  do
+  {
+    std::cout << str << std::endl;
+  } while (std::next_permutation(std::begin(str), std::end(str)));
+}
+
+int main(int argc, char *argv[])
+{
+  std::string text = "yusuke";
+
+  print_permutation(text);
+}
+
+// 順列の意味を勘違いしていた、、、
+// 以下はただの文字列のソート
 /** 
  * 1. 文字列をfor文で回す
  * 2. 文字をバブルソート
 */
 
-int main( int argc, char *argv[] )
-{
-  std::string text = "yusuke";
-  std::string result = "";
+// int main( int argc, char *argv[] )
+// {
+//   std::string text = "yusuke";
+//   std::string result = "";
 
-  for (int i = 0; i < static_cast<int>(text.length()) - 1; i++)
-  {
-    for (int j = text.length() - 1; j > i; j--)
-    {
-      if (text[j] < text[j - 1])
-      {
-        auto tmp = text[j];
-        std::cout << j << "," << j+1 << std::endl;
-        std::cout << tmp << "," << text[j-1] << std::endl;
-        text.replace(j, 1, 1, text[j-1]);
-        std::cout << text << std::endl;
-        text.replace(j-1, 1, 1, tmp);
-        std::cout << text << std::endl;
-      }
-    }
-  }
-  std::cout << text << std::endl;
-}
+//   for (int i = 0; i < static_cast<int>(text.length()) - 1; i++)
+//   {
+//     for (int j = text.length() - 1; j > i; j--)
+//     {
+//       if (text[j] < text[j - 1])
+//       {
+//         auto tmp = text[j];
+//         std::cout << j << "," << j+1 << std::endl;
+//         std::cout << tmp << "," << text[j-1] << std::endl;
+//         text.replace(j, 1, 1, text[j-1]);
+//         std::cout << text << std::endl;
+//         text.replace(j-1, 1, 1, tmp);
+//         std::cout << text << std::endl;
+//       }
+//     }
+//   }
+//   std::cout << text << std::endl;
+// }
