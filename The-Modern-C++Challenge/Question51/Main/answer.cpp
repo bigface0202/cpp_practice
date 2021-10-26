a1d266870ad42745027e97358137175f78aa1098
Yusuke <theshoreunder@gmail.com>
2021-10-26 10:03:40 +0900

    Q51 自分の回答を作成

--- The-Modern-C++Challenge/Question51/Main/answer.cpp
+++ The-Modern-C++Challenge/Question51/Main/answer.cpp
@@ -16,8 +16,32 @@
  * 4. なければ+と国番号を付与する
 */
 
+std::vector<std::string> transform_numbers(std::vector<std::string> numbers, std::string countryCode)
+{
+  // 結果格納用のvector
+  std::vector<std::string> results;
+  
+  for (auto number : numbers){
+    // std::removeでは該当を削除するが、不要な要素が末尾に寄せられただけとなる
+    // そこで、removeで該当文字を最後尾に追いやって、毎回eraseで末尾を削除する
+    number.erase(std::remove(number.begin(), number.end(), ' '), number.end());
+    if (number.find(countryCode) == 0){
+      number = "+" + number;
+    }
+    else if (!(number.find("+" + countryCode) == 0)){
+      number = "+" + countryCode + number;
+    }
+    results.push_back(number);
+  }
+
+  return results;
+}
+
 int main( int argc, char *argv[] )
 {
+  // 国番号
+  std::string countryCode = "44";
+
   // 電話番号リスト
   std::vector<std::string> numbers{
     "07555 123456",
@@ -26,17 +50,10 @@ int main( int argc, char *argv[] )
     "44 7555 123456",
     "7555 123456"
     };
-  // 結果格納用のvector
-  std::vector<std::string> results;
   
-  // 
-  for (auto number : numbers){
-    number.erase(std::remove(number.begin(), number.end(), ' '), number.end());
-    results.push_back(number);
-  }
+  std::vector<std::string> results = transform_numbers(numbers, countryCode);
 
   for (auto number : results){
     std::cout << number << std::endl;
   }
-  
 }
