8a67b60320c7b443d03e2ef9cd64d8d7cbc4dfdc
Yusuke <theshoreunder@gmail.com>
2021-10-27 09:33:34 +0900

    Q51 解答の追加

--- The-Modern-C++Challenge/Question51/Main/answer.cpp
+++ The-Modern-C++Challenge/Question51/Main/answer.cpp
@@ -10,6 +10,43 @@
 #include <string>
 
 /** 
+ * 解答
+ * 電話番号が0で始まる場合も考慮する必要がある。
+ * 0を取り除いて、国番号を付与する
+*/
+
+bool starts_with(std::string_view str, std::string_view prefix)
+{
+  return str.find(prefix) == 0;
+}
+
+// numbersを参照引数にしているので、returnする必要がない
+void normalize_phone_numbers(std::vector<std::string>& numbers, std::string const & countryCode)
+{
+  std::transform(std::cbegin(numbers), std::cend(numbers), std::begin(numbers),
+                [countryCode](std::string const & number){
+                  std::string result;
+                  if (number.size() > 0)
+                  {
+                    if (number[0] == '0')
+                      result = "+" + countryCode + number.substr(1);
+                    else if (starts_with(number, countryCode))
+                      result = "+" + number;
+                    else if (starts_with(number, "+" + countryCode))
+                      result = number;
+                    else
+                      result = "+" + countryCode + number;
+                  }
+
+                  result.erase(
+                    std::remove_if(std::begin(result), std::end(result),
+                      [](const char ch) {return isspace(ch); }),
+                    std::end(result));
+                  return result;
+                });
+}
+
+/** 
  * 1. リストの文字列に対してスペースを取り除く
  * 2. 指定の国番号があるか確認する
  * 3. あれば、+のみ付与する
@@ -25,9 +62,14 @@ std::vector<std::string> transform_numbers(std::vector<std::string> numbers, std
     // std::removeでは該当を削除するが、不要な要素が末尾に寄せられただけとなる
     // そこで、removeで該当文字を最後尾に追いやって、毎回eraseで末尾を削除する
     number.erase(std::remove(number.begin(), number.end(), ' '), number.end());
+    // 最初に国番号のみが見つかれば+のみ付与
     if (number.find(countryCode) == 0){
       number = "+" + number;
     }
+    else if (number[0] == '0'){
+      number = "+" + countryCode + number.substr(1);
+    }
+    // +国番号が見つからなければ、+国番号を付与
     else if (!(number.find("+" + countryCode) == 0)){
       number = "+" + countryCode + number;
     }
@@ -51,6 +93,13 @@ int main( int argc, char *argv[] )
     "7555 123456"
     };
   
+  // normalize_phone_numbers(numbers, "44");
+  
+  // for (auto const & number : numbers)
+  // {
+  //   std::cout << number << std::endl;
+  // }
+
   std::vector<std::string> results = transform_numbers(numbers, countryCode);
 
   for (auto number : results){
